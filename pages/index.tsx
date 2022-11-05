37e6c018bfea3d0ce8cbf943a9b3c0383dbfb749
Yusuf Güngör <yusfgungor@gmail.com>
2022-11-05 23:47:53 +0300

    init

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main>
+        <h1>Hello</h1>
+      </main>
+    </div>
+  );
+}
